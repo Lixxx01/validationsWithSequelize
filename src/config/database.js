1b6528e43c2b848113f596656a63b99849ba828f
gonzalozevallos <gonzalo.zev@gmail.com>
2021-01-20 12:27:48 -0300

    Agrego dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "chiste",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_CONNECTION,
     "define": {
       "underscored": true
     }
